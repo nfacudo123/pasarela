684b28636c03587f9cd96ff8559ed9c08bdff4c7
nfacudo123 <67806754+nfacudo123@users.noreply.github.com>
2023-11-06 12:17:47 -0500

    push 061120231217

--- walletbtc.js
+++
@@ -1,6 +0,0 @@
-const cw = require("crypto-wallets")
-
-const wallet = cw.generateWallet("BTC")
-console.log('Nueva billetera BTC generada:');
-console.log('Dirección:', wallet.address);
-console.log('Clave privada:', wallet.privateKey);
--- wallettron.js
+++
@@ -1,6 +0,0 @@
-const { generateAccount } = require('tron-create-address')
-
-const newWallet = generateAccount();
-console.log('Nueva billetera TRX generada:');
-console.log('Dirección:', newWallet.address);
-console.log('Clave privada:', newWallet.privateKey);
